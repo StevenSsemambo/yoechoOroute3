9830ea7d2833119295219e3026b28aa3e0b65c91
StevenSsemambo <rhesusfactor848@gmail.com>
2026-04-30 16:37:48 +0300

    Update db.js

--- db.js
+++ db.js
@@ -24,6 +24,9 @@
 const SUPABASE_URL      = process.env.SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
+console.log("SUPABASE_URL:", SUPABASE_URL)
+console.log("SUPABASE_KEY:", SUPABASE_ANON_KEY)
+
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin":  "*",
   "Access-Control-Allow-Headers": "Content-Type",
